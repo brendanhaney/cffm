34286bed07bdd9298804871f994f752bfeeb03eb
Bren <bdon77@outlook.com>
2026-05-07 12:53:50 -0400

    config.h cleanup

--- config.h
+++ config.h
@@ -6,26 +6,6 @@
 #define VER_PATCH 0
 #define VER_PREFIX "-dev"
 
-/*you shouldn't modify these*/
-#define TERMCOLOR -1
-#define RED     1
-#define GREEN   2
-#define BLUE    3
-#define CYAN    4
-#define PURPLE  5
-#define YELLOW  6
-#define WHITE   7
-#define BLACK   8
-
-/*window modes*/
-#define DIR_MODE      0
-#define PREVIEW_MODE  1
-#define BOX_MODE      2
-#define CMD_MODE      3
-#define INFO_MODE     4
-#define RN_MODE       5
-#define CONFIRM_MODE  6
-
 /*********************************\
 * set your desired values below.  *
 \*********************************/
--- display.h
+++ display.h
@@ -4,6 +4,26 @@
 #define MAXLINEBUFFER 256
 #define MAXPREVIEWSIZE 4096
 
+/*you shouldn't modify these*/
+#define TERMCOLOR -1
+#define RED     1
+#define GREEN   2
+#define BLUE    3
+#define CYAN    4
+#define PURPLE  5
+#define YELLOW  6
+#define WHITE   7
+#define BLACK   8
+
+/*window modes*/
+#define DIR_MODE      0
+#define PREVIEW_MODE  1
+#define BOX_MODE      2
+#define CMD_MODE      3
+#define INFO_MODE     4
+#define RN_MODE       5
+#define CONFIRM_MODE  6
+
 #include "data.h"
 
 #include <ncurses.h>
